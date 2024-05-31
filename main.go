bee13b02db27325c2b3490db0df28d98ff1bd68a
karimbaggari <karimbaggari8@gmail.com>
2024-05-31 21:45:19 +0100

    add the select channels

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	myChannel := make(chan string)
+	anotherChannel := make(chan string)
 
+	go func() {
+		anotherChannel <- "data 2"
+	}()
 	go func() {
 		myChannel <- "data"
 	}()
+	
+	time.Sleep(time.Second * 2)
 
-	msg := <-myChannel
+	select {
+		case msgFromMyChannel := <- myChannel:
+            fmt.Println("the message", msgFromMyChannel)
+		case msgFromAnotherChannel := <- anotherChannel:
+            fmt.Println("the message", msgFromAnotherChannel)
+        default:
+            fmt.Println("no message", myChannel)
+	}
 
-	fmt.Println("the message", msg)
 }
